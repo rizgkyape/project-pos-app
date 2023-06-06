e2f153087c93b0e7140a76c9805f8a4c2a031db9
rizgkyape <rizkyanandaputra811@gmail.com>
2023-06-06 15:16:33 +0700

    PPM4-5

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,54 +1,53 @@
-const db = require("./../models");
+const db = require('./../models');
 const Product = db.Product;
+const { Op } = require('sequelize');
 
 module.exports = {
-  productsList: async (req, res) => {
-    try {
-      const result = await Product.findAll({});
+	productsList: async (req, res) => {
+		try {
+			let where = undefined;
+			let order = undefined;
+			const { name, category, sortBy, sort } = req.query;
+			console.log(name);
+			console.log(category);
 
-      res.status(200).send({
-        success: true,
-        message: "fetch success",
-        data: result,
-      });
-    } catch (error) {
-      res.status(500).send({
-        success: false,
-        message: error.message,
-        data: null,
-      });
-    }
-  },
-  filterProductByName: async (req, res) => {
-    try {
-      const { name } = req.query;
+			if (name) {
+				where = {
+					name: { [Op.like]: `%${name}%` },
+				};
+			}
 
-      const result = await Product.findAll({
-        where: {
-          name: { [Op.like]: `%${name}%` },
-        },
-      });
-      
-      console.log(result);
+			if (category) {
+				if (name) {
+					where['categoryId'] = category;
+				} else {
+					where = {
+						categoryId: category,
+					};
+				}
+			}
 
-      if (result.length > 0) {
-        res.status(200).send({
-          success: true,
-          message: "Search product by name success!",
-          data: result,
-        });
-      } else {
-        res.status(404).send({
-          success: false,
-          message: "Product not found!",
-        });
-      }
-    } catch (error) {
-      res.status(400).send({
-        success: false,
-        message: error.message,
-        data: null,
-      });
-    }
-  },
+			if (sortBy && sort) {
+				order = [[`${sortBy}`, `${sort}`]];
+			}
+
+			let result = await Product.findAll({
+				where: where,
+				order: order,
+				include: [{ model: db.ProductCategory }],
+			});
+
+			return res.status(200).send({
+				success: true,
+				message: 'fetch success',
+				data: result,
+			});
+		} catch (error) {
+			res.status(500).send({
+				success: false,
+				message: error.message,
+				data: null,
+			});
+		}
+	},
 };
